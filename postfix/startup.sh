acec2ff3fe03111a872a792c0e83a3edc394c1d3
rkorytkowski <rafal.korytkowski@gmail.com>
2023-10-18 11:46:16 +0200

    Adjusting postfix

--- postfix/startup.sh
+++ postfix/startup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-echo "bind_pw = $LDAP_BIND_PASSWORD" >> /etc/postfix/ldap-aliases.cf
+echo "bind_pw = $LDAP_BIND_PASSWORD
+server_host = $LDAP_HOST
+server_port = $LDAP_PORT
+" >> /etc/postfix/ldap-aliases.cf
 echo "[$SMTP_SERVER]:$SMTP_PORT $SMTP_USERNAME:$SMTP_PASSWORD" > /etc/postfix/sasl_passwd
 chmod 600 /etc/postfix/sasl_passwd
 postmap /etc/postfix/sasl_passwd
