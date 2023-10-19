55010f8c0fea60db9b7d1e06cce207a35acad04f
rkorytkowski <rafal.korytkowski@gmail.com>
2023-10-19 15:16:10 +0200

    Adding mynetworks config

--- postfix/startup.sh
+++ postfix/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Adjusting postfix configuration"
-envsubst '$POSTFIX_HOSTNAME,$SMTP_SERVER,$SMTP_PORT' < /etc/postfix/main.cf > /etc/postfix/main.cf
+envsubst '$POSTFIX_HOSTNAME,$POSTFIX_MYNETWORKS,$SMTP_SERVER,$SMTP_PORT' < /etc/postfix/main.cf > /etc/postfix/main.cf
 envsubst '$LDAP_USER,$LDAP_BIND_PASSWORD,$LDAP_HOST,$LDAP_PORT' < /etc/postfix/ldap-aliases.cf > /etc/postfix/ldap-aliases.cf
 
 echo "Adding SASL authentication configuration"
