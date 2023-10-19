0e0604275db5f386582f1f4655098327f26fe6ce
rkorytkowski <rafal.korytkowski@gmail.com>
2023-10-19 13:05:04 +0200

    Adjusting for prod use

--- postfix/startup.sh
+++ postfix/startup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-echo "bind_pw = $LDAP_BIND_PASSWORD
-server_host = $LDAP_HOST
-server_port = $LDAP_PORT
-" >> /etc/postfix/ldap-aliases.cf
-echo "[$SMTP_SERVER]:$SMTP_PORT $SMTP_USERNAME:$SMTP_PASSWORD" > /etc/postfix/sasl_passwd
-chmod 600 /etc/postfix/sasl_passwd
+echo "Adjusting postfix configuration"
+envsubst '$POSTFIX_HOSTNAME,$SMTP_SERVER,$SMTP_PORT' < /etc/postfix/main.cf > /etc/postfix/main.cf
+envsubst '$LDAP_BIND_PASSWORD,$LDAP_HOST,$LDAP_PORT' < /etc/postfix/ldap-aliases.cf > /etc/postfix/ldap-aliases.cf
+
+echo "Adding SASL authentication configuration"
+echo "[${SMTP_SERVER}]:${SMTP_PORT} ${SMTP_USERNAME}:${SMTP_PASSWORD}" > /etc/postfix/sasl_passwd
 postmap /etc/postfix/sasl_passwd
+
+chmod 600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
 
 postfix start-fg
