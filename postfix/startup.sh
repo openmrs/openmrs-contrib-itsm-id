5b4b906c51ede3e33a3b7e1ca4dd70a10cdbbcf2
rkorytkowski <rafal.korytkowski@gmail.com>
2023-10-24 11:16:45 +0200

    Fixing smtp incoming auth

--- postfix/startup.sh
+++ postfix/startup.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
 echo "Adjusting postfix configuration"
-envsubst '$POSTFIX_HOSTNAME,$POSTFIX_MYNETWORKS,$SMTP_SERVER,$SMTP_PORT' < /etc/postfix/main.cf > /etc/postfix/main.cf
+envsubst '$POSTFIX_HOSTNAME,$POSTFIX_MYNETWORKS,$POSTFIX_TLS_CRT_FILENAME,$POSTFIX_TLS_KEY_FILENAME,$SMTP_SERVER,$SMTP_PORT' < /etc/postfix/main.cf > /etc/postfix/main.cf
 envsubst '$LDAP_USER,$LDAP_BIND_PASSWORD,$LDAP_HOST,$LDAP_PORT' < /etc/postfix/ldap-aliases.cf > /etc/postfix/ldap-aliases.cf
 
-echo "Adding SASL authentication configuration"
+echo "Adding INCOMING SASL authentication config"
+mkdir -p /etc/sasl2
+echo "pwcheck_method: auxprop
+auxprop_plugin: sasldb
+mech_list: PLAIN LOGIN CRAM-MD5 DIGEST-MD5 NTLM" > /etc/sasl2/smtpd.conf
+echo "$POSTFIX_PASSWORD" | saslpasswd2 -c -p -u "$POSTFIX_HOSTNAME" "$POSTFIX_USERNAME"
+
+echo "Adding OUTGOING SASL authentication config"
 echo "[${SMTP_SERVER}]:${SMTP_PORT} ${SMTP_USERNAME}:${SMTP_PASSWORD}" > /etc/postfix/sasl_passwd
 postmap /etc/postfix/sasl_passwd
-
-chmod 600 /etc/postfix/sasl_passwd /etc/postfix/sasl_passwd.db
+chmod 600 /etc/postfix/sasl_passwd
 
 postfix start-fg
